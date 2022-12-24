aa1d89a2ca95dc107f901a686f38772cddc95584
PavelBazilevich <102550003+PavelBazilevich@users.noreply.github.com>
2022-12-24 23:05:55 +0200

    test

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, TextInput } from "react-native";
 import RegistrationScreen from "./Screens/RegistrationScreen";
-import LoginScreen from "./Screens/LoginScreen";
+// import LoginScreen from "./Screens/LoginScreen";
+// import * as SplashScreen from "expo-splash-screen";
+import { useFonts } from "expo-font";
+import React, { useCallback } from "react";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Roboto-Regular": require("./assets/Fonts/Roboto-Regular.ttf"),
+    "Roboto-Medium": require("./assets/Fonts/Roboto-Medium.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
+    <View style={styles.container} onLayoutRootView={onLayoutRootView}>
       <RegistrationScreen />
       {/* <LoginScreen /> */}
       <StatusBar style="auto" />
@@ -18,5 +39,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     justifyContent: "center",
+    // fontFamily: "Roboto-Regular",
   },
 });
--- Screens/RegistrationScreen.js
+++ Screens/RegistrationScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import {
   StyleSheet,
   View,
@@ -10,13 +10,17 @@ import {
   KeyboardAvoidingView,
   Platform,
   ImageBackground,
+  Image,
 } from "react-native";
-// import styles from "./RegistrationStyles";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+SplashScreen.preventAutoHideAsync();
 
 export default function RegistrationScreen() {
   const [email, setEmail] = useState("");
   const [login, setLogin] = useState("");
   const [password, setPassword] = useState("");
+  // const [isReady, setIsReady] = useState(false);
 
   const emailHandler = (text) => setEmail(text);
   const loginHandler = (text) => setLogin(text);
@@ -29,49 +33,70 @@ export default function RegistrationScreen() {
     setPassword("");
   };
 
+  const [fontsLoaded] = useFonts({
+    "Roboto-Regular": require("../assets/Fonts/Roboto-Regular.ttf"),
+    "Roboto-Medium": require("../assets/Fonts/Roboto-Medium.ttf"),
+  });
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <View style={styles.container}>
+      <View style={styles.container} onLayout={onLayoutRootView}>
         <ImageBackground
-          source={{ uri: "photo-1536528087222-ef43dd3bb0f3.jpg" }}
-          style={{ width: 375, height: 812 }}
+          source={require("../assets/Images/PhotoBG.jpg")}
+          style={styles.imageBg}
         >
-          <KeyboardAvoidingView
-            behavior={Platform.OS == "ios" ? "padding" : "height"}
-          >
-            <Text style={styles.title}>Регистрация</Text>
-            <TextInput
-              value={login}
-              onChangeText={loginHandler}
-              placeholder="Логин"
-              style={styles.input}
-            />
-            <TextInput
-              value={email}
-              onChangeText={emailHandler}
-              placeholder="Адрес электронной почты"
-              style={styles.input}
-            />
-            <TextInput
-              value={password}
-              onChangeText={passwordHandler}
-              placeholder="Пароль"
-              secureTextEntry={true}
-              style={styles.input}
-            />
-            {/* <Button
-            title={"Зарегистрироваться"}
-            style={styles.btn}
-            onPress={onLogin}
-          /> */}
-            <TouchableOpacity
-              activeOpacity={0.8}
-              style={styles.btn}
-              onPress={onLogin}
+          <View style={styles.contentBox}>
+            <KeyboardAvoidingView
+              behavior={Platform.OS == "ios" ? "padding" : "height"}
             >
-              <Text style={styles.titleBtn}>Зарегистрироваться</Text>
-            </TouchableOpacity>
-          </KeyboardAvoidingView>
+              <View style={styles.avatarBox}>
+                <View style={styles.avatarThumb}>
+                  <TouchableOpacity activeOpacity={0.8} style={styles.iconBtn}>
+                    <Text style={styles.iconAdd}>+</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+              <Text style={styles.title}>Регистрация</Text>
+              <TextInput
+                value={login}
+                onChangeText={loginHandler}
+                placeholder="Логин"
+                style={styles.input}
+              />
+              <TextInput
+                value={email}
+                onChangeText={emailHandler}
+                placeholder="Адрес электронной почты"
+                style={styles.input}
+              />
+              <TextInput
+                nativeID="password"
+                value={password}
+                onChangeText={passwordHandler}
+                placeholder="Пароль"
+                secureTextEntry={true}
+                style={styles.input}
+              />
+              <Text style={styles.show}>Показать</Text>
+              <TouchableOpacity
+                activeOpacity={0.8}
+                style={styles.btn}
+                onPress={onLogin}
+              >
+                <Text style={styles.titleBtn}>Зарегистрироваться</Text>
+              </TouchableOpacity>
+              <Text style={styles.footerTitle}>Уже есть аккаунт? Войти</Text>
+            </KeyboardAvoidingView>
+          </View>
         </ImageBackground>
       </View>
     </TouchableWithoutFeedback>
@@ -81,10 +106,9 @@ export default function RegistrationScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
   },
   input: {
+    position: "relative",
     width: 343,
     height: 50,
     borderRadius: 8,
@@ -95,9 +119,16 @@ const styles = StyleSheet.create({
     padding: 10,
     marginBottom: 16,
   },
-
+  show: {
+    position: "relative",
+    top: -50,
+    left: 256,
+    fontSize: 16,
+    color: "#1b4371",
+  },
   btn: {
     marginTop: 43,
+    marginBottom: 16,
     width: 343,
     height: 51,
     backgroundColor: "#FF6C00",
@@ -113,9 +144,57 @@ const styles = StyleSheet.create({
     fontSize: 16,
   },
   title: {
+    fontFamily: "Roboto-Regular",
     textAlign: "center",
     color: "#212121",
     fontSize: 32,
     marginBottom: 33,
+    marginTop: 92,
+  },
+  imageBg: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "flex-end",
+  },
+  contentBox: {
+    position: "relative",
+    alignItems: "center",
+    justifyContent: "center",
+    justifyContent: "flex-end",
+    backgroundColor: "#fff",
+    borderTopRightRadius: 25,
+    borderTopLeftRadius: 25,
+  },
+  avatarBox: {
+    position: "absolute",
+    top: -60,
+    left: "30%",
+  },
+  avatarThumb: {
+    width: 120,
+    height: 120,
+    borderRadius: 16,
+    backgroundColor: "#f6f6f6",
+  },
+  iconBtn: {
+    position: "relative",
+    width: 25,
+    height: 25,
+  },
+  iconAdd: {
+    display: "flex",
+    position: "absolute",
+    top: 81,
+    left: 109,
+    fontSize: 37,
+    color: "#FF6C00",
+  },
+  footerTitle: {
+    fontFamily: "Roboto-Regular",
+    marginTop: 16,
+    marginBottom: 78,
+    fontSize: 16,
+    textAlign: "center",
+    color: "#1b4371",
   },
 });
